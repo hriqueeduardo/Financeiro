49dfd8a6b0f2dbbc49cd9f84620f34f5826fc361
hriqueeduardo <hriqueeduardo@gmail.com>
2024-07-13 19:30:10 -0300

    implementação da LoginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+
     public function index()
     {
         return view('login.index');
     }
+
+    public function auth(Request $request)
+    {
+        $credenciais = $request->validate([
+            'usuario' => 'required',
+            'senha' => 'required'
+        ]);
+
+
+        if (Auth::attempt($credenciais)) {
+
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+
+        } else {
+            
+            return redirect()->back()->with('usuario ou senha ínvalidas');
+        }
+    }
+
 }
